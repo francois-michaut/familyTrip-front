136b8b4bca0148e25569423e23376e88adfd7dac
francois-michaut <f.michaut@orange.fr>
2022-08-05 14:48:59 +0200

    Mise en place Activity page version mobile and desktop

--- src/components/Activity/index.js
+++ src/components/Activity/index.js
@@ -1,12 +1,32 @@
 // import
 import './activity.scss';
-
 // Composant
 
 function Activity() {
   return (
     <main className="activity">
-      <h2 className="activity__title"> Créer une activité</h2>
+      <h2 className="activity__title"> Programmer une activité</h2>
+      <div className="activity__form">
+        <div className="mb-3">
+          <label htmlFor="activity-type" className="form-label">Type d'activités</label>
+          <input type="text" className="form-control" id="activity-type" placeholder="randonnée" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="activity-type" className="form-label">Date</label>
+          <input type="date" className="form-control" id="activity-type" placeholder="01/01/2024" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="activity-type" className="form-label">Durée de l'activité</label>
+          <input type="text" className="form-control" id="activity-type" placeholder="1h" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="activity-type" className="form-label">Lieu</label>
+          <input type="lieu" className="form-control" id="activity-type" placeholder="Nice" />
+        </div>
+        <div className="activity__form__button">
+          <button type="button" className="btn btn-success">Créer une activité</button>
+        </div>
+      </div>
     </main>
   );
 }
