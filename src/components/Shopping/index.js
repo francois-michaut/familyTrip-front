cc65a487fc5c63f0274c273b94eba2ffe207c04c
francois-michaut <f.michaut@orange.fr>
2022-08-05 15:32:56 +0200

    Mise en place de Shopping version mobile and desktop

--- src/components/Shopping/index.js
+++ src/components/Shopping/index.js
@@ -4,8 +4,29 @@ import './shopping.scss';
 // Composant
 function Shopping() {
   return (
-    <main>
-      <h2>Salut</h2>
+    <main className="shopping">
+      <h2 className="shopping__title">Créer une liste de courses</h2>
+      <div className="shopping__list">
+        <div className="mb-3">
+          <input type="text" className="form-control" id="activity-type" placeholder="1er ingrédient" />
+        </div>
+        <div className="mb-3">
+          <input type="text" className="form-control" id="activity-type" placeholder="2e ingrédient" />
+        </div>
+        <div className="mb-3">
+          <input type="text" className="form-control" id="activity-type" placeholder="3e ingrédient" />
+        </div>
+        <div className="mb-3">
+          <input type="text" className="form-control" id="activity-type" placeholder="4e ingrédient" />
+        </div>
+        <div className="mb-3">
+          <input type="text" className="form-control" id="activity-type" placeholder="5e ingrédient" />
+        </div>
+        <div className="shopping__list__buttons">
+          <button className=" btn btn-primary" type="button">Ajouter un ingrédient</button>
+          <button className=" btn btn-success" type="submit">Valider la liste</button>
+        </div>
+      </div>
     </main>
   );
 }
