c5ad9e6c693aa4b84ef985443684cb479aa8bcee
francois-michaut <f.michaut@orange.fr>
2022-08-30 13:00:42 +0200

    Création d'une nouvelle tribu et envoi au back ok

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -1,28 +1,71 @@
 import axios from 'axios';
 
 import {
+  GET_TRIBES,
   GET_USER,
   loadUsers,
+  POST_TRIBE_NAME,
+  saveTribes,
 } from '../actions/users';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost/Family-Trip/familyTrip-back/user',
+  baseURL: 'http://localhost:3000/',
 });
 
 const apiMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case GET_USER: {
       axiosInstance
-        .get('roadworks')
+        .get('Api/members')
         .then(
           (response) => {
-            console.log(response.data);
             store.dispatch(loadUsers(response.data));
           },
         )
         .catch(
-          () => console.log('erreur api'),
+          (error) => console.log(error),
         );
+      next(action);
+      break;
+    }
+
+    case GET_TRIBES: {
+      axiosInstance
+        .get('Api/tribes')
+        .then(
+          (response) => {
+            store.dispatch(saveTribes(response.data));
+          },
+        )
+        .catch(
+          (error) => console.log(error),
+        );
+      next(action);
+      break;
+    }
+
+    case POST_TRIBE_NAME: {
+      const state = store.getState();
+      const { tribeName } = state.tribe;
+      console.log(tribeName);
+      axiosInstance
+        .post(
+          'Api/createTribePost',
+          {
+            tribeName: tribeName,
+          },
+          // {
+          //   'Content-Type': 'application/json',
+          // },
+        )
+        .then((response) => {
+          const tribe = response.config.data;
+
+          console.log(tribe);
+        })
+        .catch(() => {
+          console.log('erreur API');
+        });
       next(action);
       break;
     }
